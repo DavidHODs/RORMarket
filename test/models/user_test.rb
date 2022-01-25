b08fa71b41f7f8f3f666138ca35ee132abe20b8f
DavidHODs <davidoluwatobi41@gmail.com>
2022-01-25 20:38:54 +0000

    user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user with a valid email should be valid" do
+    user = User.new(email: "test@email.com", password_digest: "password")
+    assert user.valid?
+  end
+
+  test "user with an invalid email should be invalid" do
+    user = User.new(email: "test.email.com", password_digest: "password")
+    assert_not user.valid?
+  end
+
+  test "email should be unique" do
+    otherUser = users(:one)
+    user = User.new(otherUser.email, password: "password")
+    assert_not user.valid?
+  end
 end

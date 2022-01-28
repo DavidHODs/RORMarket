df22d794c873aa4731138a3e30ea2dc9684d0631
DavidHODs <davidoluwatobi41@gmail.com>
2022-01-28 12:37:27 +0000

    update user

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -25,6 +25,16 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       post api_v1_users_url, params: { user: { email: @user.email, password: "123456" } }, as: :json 
     end
     assert_response :unprocessable_entity
+  end
+
+  test "should update user" do 
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, paswword: "password" } }, as: :json
+    assert_response :success 
+  end
+
+  test "should not update user when invalid params are sent" do 
+    patch api_v1_user_url(@user), params: { user: { email: "bad_email", password: "password" } }, as: :json 
+    assert_response :unprocessable_entity
   end 
 
 end

69cc61afb0ee2e63058e689e35eb0e0acb74a262
DavidHODs <davidoluwatobi41@gmail.com>
2022-01-30 20:37:53 +0000

    added seed dataset

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ end
 gem "bcrypt", "~> 3.1"
 
 gem "jwt", "~> 2.3"
+
+gem "faker", "~> 2.19"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.destroy_all
+User.delete_all 
+user = User.create! email: "test1@gmail.com", password: "password"
+puts "created a new user: #{user.email}"
+
+5.times do 
+    Product.destroy_all
+    User.delete_all 
+    user = User.create! email: Faker::Internet.email, password: "locadex1234"
+    puts "created a new user: #{user.email}"
+
+    5.times do
+        product = Product.create!(
+            title: Faker::Commerce.product_name, 
+            price: rand(1.0..100.0),
+            published: true,
+            user_id: user.id 
+        )
+        puts "#{user.email} created a product #{product.title}"
+    end 
+end
+
+
+
